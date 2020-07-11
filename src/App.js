6242f990546d3b778c5df28186ea346254280010
April Chi <aprilchi4@gmail.com>
2020-07-12 01:04:43 +1000

    prettified files

--- src/App.js
+++ src/App.js
@@ -3,35 +3,41 @@ import "./App.css";
 
 // components
 import Introduction from "./components/Introduction";
-import QuizPage from './components/QuizPage'; 
+import QuizPage from "./components/QuizPage";
 
 class App extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        showQuestion : false,
-        showAnimation: false
-      }
+    this.state = {
+      showQuestion : false,
+      showAnimation: false,
+    };
   }
 
   clickStartButton = () => {
     this.setState({ showAnimation: true });
-    setTimeout(() => {this.setState({ showQuestion: true })},
-    2000)
-  }
-  
+    setTimeout(() => {
+      this.setState({ showQuestion: true });
+    }, 2000);
+  };
+
   renderQuestion = () => {
-    return <QuizPage />
-  }
+    return <QuizPage />;
+  };
 
   renderIntro = () => {
-    return <Introduction handleClick={() => this.clickStartButton()} animation={this.state.showAnimation}/>
-  }
+    return (
+      <Introduction
+        handleClick = {() => this.clickStartButton()}
+        animation   = {this.state.showAnimation}
+      />
+    );
+  };
 
   render() {
     let question = this.state.showQuestion;
 
-    if(question) {
+    if (question) {
       return this.renderQuestion();
     } else {
       return this.renderIntro();
