d3968153e58124af1139cc77b1ba9cde48c9e525
April Chi <aprilchi4@gmail.com>
2020-07-12 20:01:08 +1000

    changed it to how techy are you

--- src/utils/FlexComponents.js
+++ src/utils/FlexComponents.js
@@ -9,9 +9,6 @@ export const Row = styled.div`
     border-style   : 3px solid #000000;
 `;
 
-
-
-
 export const Col = styled.div`
     flex        : ${(props) => props.size};
     border-style: 3px solid #000000;
